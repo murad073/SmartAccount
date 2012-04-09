66745984bf2d20f8131c7d5ded83cee5de70a644
Moshiur Rahman <murad073@gmail.com>
2012-04-09 07:57:23 +0600

    messaging corrected

--- Project Source/trunk/BLL.Messaging/MessageEnums.cs
+++ Project Source/trunk/BLL.Messaging/MessageEnums.cs
@@ -1,33 +1,33 @@
-﻿namespace BLL.Messaging
-{
-    public enum ErrorMessage
-    {
-        NoProjectSelected,
-        NoHeadSelected,
-        AmountCannotBeZero,
-        ContraTypeIsNotSelected,
-        JVDebitOrCreditNotSelected,
-        UnknownProblemArise,
-        InvalidProject,
-        InvalidHeadForProject,
-        DebitOrCreditAmountIsInvalid,
-        VoucherBalanceIsNotZero
-    }
+﻿//namespace BLL.Messaging
+//{
+//    public enum ErrorMessage
+//    {
+//        NoProjectSelected,
+//        NoHeadSelected,
+//        AmountCannotBeZero,
+//        ContraTypeIsNotSelected,
+//        JVDebitOrCreditNotSelected,
+//        UnknownProblemArise,
+//        InvalidProject,
+//        InvalidHeadForProject,
+//        DebitOrCreditAmountIsInvalid,
+//        VoucherBalanceIsNotZero
+//    }
 
-    public enum SuccessMessage
-    {
-        //TemporaryRecordsAdded,
-        VoucherPostedSuccessfully
-    }
+//    public enum SuccessMessage
+//    {
+//        //TemporaryRecordsAdded,
+//        VoucherPostedSuccessfully
+//    }
 
-    public enum WarningMessage
-    {
-        NoFixedAssetParticularNameFound,
-        ZeroDepreciationProvidedForFixedAsset
-    }
+//    public enum WarningMessage
+//    {
+//        NoFixedAssetParticularNameFound,
+//        ZeroDepreciationProvidedForFixedAsset
+//    }
 
-    public enum InformationMessage
-    {
-        NoChequeOrBankInfo
-    }
-}
+//    public enum InformationMessage
+//    {
+//        NoChequeOrBankInfo
+//    }
+//}
