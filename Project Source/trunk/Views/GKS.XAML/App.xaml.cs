d602b53385906b8fb62aeab880879e2a63b87e0d
unknown <MRahman@DACW0001.internal.imsglobal.com>
2012-04-27 20:36:21 +0600

    simple change added

--- Project Source/trunk/Views/GKS.XAML/App.xaml.cs
+++ Project Source/trunk/Views/GKS.XAML/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace GKS.XAML
 {
@@ -12,5 +14,16 @@ namespace GKS.XAML
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            base.DispatcherUnhandledException += AppDispatcherUnhandledException;
+        }
+
+        static void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            File.WriteAllText("errorlog.txt", e.Exception.ToString());
+            e.Handled = false;
+        }
     }
 }
+
