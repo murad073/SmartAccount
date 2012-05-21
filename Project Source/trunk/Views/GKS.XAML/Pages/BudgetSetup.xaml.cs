e142d6e5ac1ed62c06328fd1eb37b859b062f66a
Moshiur Rahman <murad073@gmail.com>
2012-05-21 20:45:25 +0600

    little

--- Project Source/trunk/Views/GKS.XAML/Pages/BudgetSetup.xaml.cs
+++ Project Source/trunk/Views/GKS.XAML/Pages/BudgetSetup.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GKS.Model.ViewModels;
 
 namespace GKS.XAML.Pages
 {
@@ -21,6 +22,7 @@ namespace GKS.XAML.Pages
         public BudgetSetup()
         {
             InitializeComponent();
+            DataContext = new BudgetSetupModel();
         }
     }
 }
